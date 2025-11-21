523bed42413992d20aee83e348f0c90e0e2fd9a1
Ahmedatef5O5 <ahmedateif11@gmail.com>
2025-11-21 22:22:58 +0200

    latest transaction part two

--- lib/widgets/latest_transaction_list_view.dart
+++ lib/widgets/latest_transaction_list_view.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/models/user_info_model.dart';
+import 'package:responsive_dash_board/utils/app_images.dart';
+import 'package:responsive_dash_board/widgets/user_info_list_tile.dart';
 
 class LatestTransactionListView extends StatelessWidget {
   const LatestTransactionListView({super.key});
-
+  static const items = [
+    UserInfoModel(
+      image: Assets.imagesAvatar1,
+      title: 'Madrani Andi',
+      subTitle: 'Madraniadi20@gmail',
+    ),
+    UserInfoModel(
+      image: Assets.imagesAvatar2,
+      title: 'Madrani Andi',
+      subTitle: 'Madraniadi20@gmail',
+    ),
+    UserInfoModel(
+      image: Assets.imagesAvatar3,
+      title: 'Madrani Andi',
+      subTitle: 'Madraniadi20@gmail',
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
-    return Row();
+    return SizedBox(
+      height: 300,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return UserInfoListtile(userInfoModel: items[index]);
+        },
+      ),
+    );
   }
 }
--- lib/widgets/user_info_list_tile.dart
+++ lib/widgets/user_info_list_tile.dart
@@ -1,24 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:responsive_dash_board/models/user_info_model.dart';
 import 'package:responsive_dash_board/utils/app_Styles.dart';
 
 class UserInfoListtile extends StatelessWidget {
-  const UserInfoListtile({
-    super.key,
-    required this.image,
-    required this.title,
-    required this.subTitle,
-  });
-  final String image, title, subTitle;
+  const UserInfoListtile({super.key, required this.userInfoModel});
+  final UserInfoModel userInfoModel;
   @override
   Widget build(BuildContext context) {
     return Card(
       color: Color(0xfffafafa),
       elevation: 0, // to clear any shadow
       child: ListTile(
-        leading: SvgPicture.asset(image),
-        title: Text(title, style: AppStyles.styleBold16),
-        subtitle: Text(subTitle, style: AppStyles.styleRegular12),
+        leading: SvgPicture.asset(userInfoModel.image),
+        title: Text(userInfoModel.title, style: AppStyles.styleBold16),
+        subtitle: Text(userInfoModel.subTitle, style: AppStyles.styleRegular12),
       ),
     );
   }
