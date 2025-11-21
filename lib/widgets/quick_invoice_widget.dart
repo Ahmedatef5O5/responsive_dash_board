daf8e3765ce4c56b57d66841db9c13d667fb5500
Ahmedatef5O5 <ahmedateif11@gmail.com>
2025-11-21 22:06:00 +0200

    latest transaction part one

--- lib/widgets/quick_invoice_widget.dart
+++ lib/widgets/quick_invoice_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/custom_background_container.dart';
+import 'package:responsive_dash_board/widgets/latest_transaction.dart';
 import 'package:responsive_dash_board/widgets/quick_invoice_header.dart';
 
 class QuickInvoice extends StatelessWidget {
@@ -8,7 +9,7 @@ class QuickInvoice extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CustomBackgroundContainer(
-      child: Column(children: [QuickInvoiceHeader()]),
+      child: Column(children: [QuickInvoiceHeader(), LatestTransaction()]),
     );
   }
 }
