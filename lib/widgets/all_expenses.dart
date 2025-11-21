a6487cfb7b1a8d6f728007d1f0678ff699f13bf2
Ahmedatef5O5 <ahmedateif11@gmail.com>
2025-11-21 21:32:52 +0200

    create custom background widget (refactor)

--- lib/widgets/all_expenses.dart
+++ lib/widgets/all_expenses.dart
@@ -1,33 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/all_expenses_header.dart';
 import 'package:responsive_dash_board/widgets/all_expenses_item_list_view.dart';
+import 'package:responsive_dash_board/widgets/custom_background_container.dart';
 
 class AllExpenses extends StatelessWidget {
   const AllExpenses({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(20),
-      decoration: ShapeDecoration(
-        color: Colors.white,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadiusGeometry.circular(12),
-        ),
-      ),
+    return CustomBackgroundContainer(
       child: Column(
         children: [
           AllExpensesHeader(),
           SizedBox(height: 16),
           Row(children: [Expanded(child: AllExpensesItemListView())]),
-          // AllExpensesItem(
-          //   itemModel: AllExpensesItemModel(
-          //     image: Assets.imagesIncome,
-          //     title: "Income",
-          //     date: "April 2022",
-          //     price: r"$20,129",
-          //   ),
-          // ),
         ],
       ),
     );
