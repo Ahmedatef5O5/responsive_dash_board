c49150772c13a0adf6487b9c67ac01afc0482b2c
Ahmedatef5O5 <ahmedateif11@gmail.com>
2025-11-22 13:16:59 +0200

    title text field

--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/utils/app_styles.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key});
-
+  const CustomTextField({super.key, required this.hint});
+  final String hint;
   @override
   Widget build(BuildContext context) {
     return TextField(
       cursorColor: Colors.black54,
       decoration: InputDecoration(
+        hintStyle: AppStyles.styleRegular16.copyWith(color: Color(0xffaaaaaa)),
+        hintText: hint,
         fillColor: Color(0xfffafafa),
         filled: true,
         border: buildBorder(),
--- lib/widgets/quick_invoice_widget.dart
+++ lib/widgets/quick_invoice_widget.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/custom_background_container.dart';
-import 'package:responsive_dash_board/widgets/custom_text_field.dart';
 import 'package:responsive_dash_board/widgets/latest_transaction.dart';
 import 'package:responsive_dash_board/widgets/quick_invoice_header.dart';
+import 'package:responsive_dash_board/widgets/title_text_field.dart';
 
 class QuickInvoice extends StatelessWidget {
   const QuickInvoice({super.key});
@@ -17,7 +17,7 @@ class QuickInvoice extends StatelessWidget {
           SizedBox(height: 24),
           LatestTransaction(),
           Divider(height: 48),
-          CustomTextField(),
+          TitleTextField(title: 'Customer Name', hint: "Type Customer Name"),
         ],
       ),
     );
