e774b716b007f29d8ec9e94f7bbb5bc8f8217b2c
Ahmedatef5O5 <ahmedateif11@gmail.com>
2025-11-22 18:00:17 +0200

    create my card part one

--- lib/layouts/dashboard_desktop_layout.dart
+++ lib/layouts/dashboard_desktop_layout.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/all_expenses_and_quick_invoice_section.dart';
 import 'package:responsive_dash_board/widgets/custom_drawer.dart';
+import 'package:responsive_dash_board/widgets/my_card.dart';
 
 class DashboardDesktopLayout extends StatelessWidget {
   const DashboardDesktopLayout({super.key});
@@ -18,6 +19,8 @@ class DashboardDesktopLayout extends StatelessWidget {
             child: AllExpensesAndQuickInvoiceSection(),
           ),
         ),
+
+        Expanded(child: MyCard()),
       ],
     );
   }
---
+++ lib/widgets/my_card.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/utils/app_images.dart';
+
+class MyCard extends StatelessWidget {
+  const MyCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 420 / 215,
+      child: Container(
+        decoration: ShapeDecoration(
+          image: DecorationImage(
+            image: AssetImage(Assets.imagesCardBackground),
+          ),
+          color: Color(0xff4eb7f2),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadiusGeometry.circular(12),
+          ),
+        ),
+      ),
+    );
+  }
+}
