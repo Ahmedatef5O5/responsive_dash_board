177cead692b6407c7aae597486a5f1c8eca1e940
Ahmedatef5O5 <ahmedateif11@gmail.com>
2025-11-22 18:18:35 +0200

    my card done

--- lib/widgets/my_card.dart
+++ lib/widgets/my_card.dart
@@ -21,6 +21,7 @@ class MyCard extends StatelessWidget {
           ),
         ),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             ListTile(
               contentPadding: const EdgeInsets.only(
@@ -37,6 +38,29 @@ class MyCard extends StatelessWidget {
                 style: AppStyles.styleMedium20.copyWith(color: Colors.white),
               ),
               trailing: SvgPicture.asset(Assets.imagesGallery),
+            ),
+            Expanded(child: const SizedBox()),
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    '0918 8124 0042 8129',
+                    style: AppStyles.styleSemiBold24.copyWith(
+                      color: Colors.white,
+                    ),
+                  ),
+
+                  Text(
+                    '12 / 20  - 124',
+                    style: AppStyles.styleRegular16.copyWith(
+                      color: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(height: 26),
+                ],
+              ),
             ),
           ],
         ),
