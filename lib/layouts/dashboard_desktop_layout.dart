85420aa9900060e4964a1ffd78e25e882b5c38b0
Ahmedatef5O5 <ahmedateif11@gmail.com>
2025-11-22 15:46:01 +0200

    simple refactor

--- lib/layouts/dashboard_desktop_layout.dart
+++ lib/layouts/dashboard_desktop_layout.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:responsive_dash_board/widgets/all_expenses.dart';
+import 'package:responsive_dash_board/widgets/all_expenses_and_quick_invoice_section.dart';
 import 'package:responsive_dash_board/widgets/custom_drawer.dart';
-import 'package:responsive_dash_board/widgets/quick_invoice_widget.dart';
 
 class DashboardDesktopLayout extends StatelessWidget {
   const DashboardDesktopLayout({super.key});
@@ -14,13 +13,9 @@ class DashboardDesktopLayout extends StatelessWidget {
         SizedBox(width: 32),
         Expanded(
           flex: 2,
-          child: Column(
-            children: [
-              SizedBox(height: 40),
-              AllExpenses(),
-              SizedBox(height: 24),
-              QuickInvoice(),
-            ],
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: AllExpensesAndQuickInvoiceSection(),
           ),
         ),
       ],
