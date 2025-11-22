59a2a8864368c0c209d3edb52f7cf135e6f1380b
Ahmedatef5O5 <ahmedateif11@gmail.com>
2025-11-22 19:44:01 +0200

    my cards page view using(Expandable Page View)

--- lib/widgets/my_card.dart
+++ lib/widgets/my_card.dart
@@ -13,6 +13,7 @@ class MyCard extends StatelessWidget {
       child: Container(
         decoration: ShapeDecoration(
           image: DecorationImage(
+            fit: BoxFit.fill,
             image: AssetImage(Assets.imagesCardBackground),
           ),
           color: Color(0xff4eb7f2),
