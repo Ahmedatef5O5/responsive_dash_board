82e0b7427b96a97b5c075937a5291ea8596b87f4
Ahmedatef5O5 <ahmedateif11@gmail.com>
2025-11-23 18:32:33 +0200

    my cards section done

--- lib/widgets/my_cards_section.dart
+++ lib/widgets/my_cards_section.dart
@@ -3,8 +3,27 @@ import 'package:responsive_dash_board/utils/app_styles.dart';
 import 'package:responsive_dash_board/widgets/dots_indicator.dart';
 import 'package:responsive_dash_board/widgets/my_cards_page_view.dart';
 
-class MyCardsSection extends StatelessWidget {
+class MyCardsSection extends StatefulWidget {
   const MyCardsSection({super.key});
+
+  @override
+  State<MyCardsSection> createState() => _MyCardsSectionState();
+}
+
+class _MyCardsSectionState extends State<MyCardsSection> {
+  late PageController pageController;
+  int currentPageIndex = 0;
+
+  @override
+  void initState() {
+    pageController = PageController();
+    pageController.addListener(() {
+      currentPageIndex = pageController.page!.round();
+      setState(() {});
+    });
+    // TODO: implement initState
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,9 +35,9 @@ class MyCardsSection extends StatelessWidget {
           child: Text('My Card', style: AppStyles.styleSemiBold20),
         ),
         SizedBox(height: 20),
-        MyCardsPageView(),
+        MyCardsPageView(pageController: pageController),
         SizedBox(height: 20),
-        DotsIndicator(),
+        DotsIndicator(currentPageIndex: currentPageIndex),
       ],
     );
   }
