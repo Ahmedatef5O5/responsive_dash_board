2042fda3a02ae68f44e1f630d33e4d59aee5a3b0
Ahmedatef5O5 <ahmedateif11@gmail.com>
2025-11-23 20:00:40 +0200

    transaction history part three

---
+++ lib/widgets/transaction_item.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/models/transaction_model.dart';
+import 'package:responsive_dash_board/utils/app_Styles.dart';
+
+class TransactionItem extends StatelessWidget {
+  const TransactionItem({super.key, required this.transactionModel});
+  final TransactionModel transactionModel;
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 0,
+      color: const Color(0xfffafafa),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadiusGeometry.circular(12),
+      ),
+      child: ListTile(
+        title: Text(
+          transactionModel.title,
+          style: AppStyles.styleBold16.copyWith(color: const Color(0xff064060)),
+        ),
+        subtitle: Text(
+          transactionModel.subTitle,
+          style: AppStyles.styleRegular16.copyWith(
+            color: const Color(0xffaaaaaa),
+          ),
+        ),
+        trailing: Text(
+          transactionModel.amount,
+          style: AppStyles.styleSemiBold20.copyWith(
+            color: transactionModel.isWithdrawal
+                ? const Color(0xfff3735e)
+                : const Color(0xff7cd87a),
+          ),
+        ),
+      ),
+    );
+  }
+}
