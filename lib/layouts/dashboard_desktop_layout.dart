b341c4f47dd33d85d586e507ab05e4e6ed189dcc
Ahmedatef5O5 <ahmedateif11@gmail.com>
2025-11-23 16:27:10 +0200

    generate dots indicator

--- lib/layouts/dashboard_desktop_layout.dart
+++ lib/layouts/dashboard_desktop_layout.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/all_expenses_and_quick_invoice_section.dart';
 import 'package:responsive_dash_board/widgets/custom_dot_indicator.dart';
 import 'package:responsive_dash_board/widgets/custom_drawer.dart';
+import 'package:responsive_dash_board/widgets/dots_indicator.dart';
 import 'package:responsive_dash_board/widgets/my_cards_page_view.dart';
 
 class DashboardDesktopLayout extends StatelessWidget {
@@ -22,7 +23,8 @@ class DashboardDesktopLayout extends StatelessWidget {
         ),
         SizedBox(width: 24),
         // Expanded(child: MyCardsPageView()),
-        CustomDotIndicator(isActive: true),
+        // CustomDotIndicator(isActive: false),
+        DotsIndicator(),
       ],
     );
   }
---
+++ lib/widgets/dots_indicator.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/widgets/custom_dot_indicator.dart';
+
+class DotsIndicator extends StatelessWidget {
+  const DotsIndicator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: List.generate(
+        3,
+        (e) => Padding(
+          padding: const EdgeInsets.only(right: 6),
+          child: CustomDotIndicator(isActive: false),
+        ),
+      ),
+    );
+  }
+}
