43363efa8e4b5cc3c2f059c8571d05a20543a060
Ahmedatef5O5 <ahmedateif11@gmail.com>
2025-11-23 16:48:06 +0200

    my cards section

--- lib/layouts/dashboard_desktop_layout.dart
+++ lib/layouts/dashboard_desktop_layout.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/all_expenses_and_quick_invoice_section.dart';
-import 'package:responsive_dash_board/widgets/custom_dot_indicator.dart';
 import 'package:responsive_dash_board/widgets/custom_drawer.dart';
-import 'package:responsive_dash_board/widgets/dots_indicator.dart';
-import 'package:responsive_dash_board/widgets/my_cards_page_view.dart';
+import 'package:responsive_dash_board/widgets/my_cards_section.dart';
 
 class DashboardDesktopLayout extends StatelessWidget {
   const DashboardDesktopLayout({super.key});
@@ -22,9 +20,7 @@ class DashboardDesktopLayout extends StatelessWidget {
           ),
         ),
         SizedBox(width: 24),
-        // Expanded(child: MyCardsPageView()),
-        // CustomDotIndicator(isActive: false),
-        DotsIndicator(),
+        Expanded(child: MyCardsSection()),
       ],
     );
   }
