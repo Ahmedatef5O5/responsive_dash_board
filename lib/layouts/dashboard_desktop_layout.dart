d969f7f53100ef43683fc6cbde8c83747d226a73
Ahmedatef5O5 <ahmedateif11@gmail.com>
2025-11-23 16:21:04 +0200

    create custom dot indicator

--- lib/layouts/dashboard_desktop_layout.dart
+++ lib/layouts/dashboard_desktop_layout.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/all_expenses_and_quick_invoice_section.dart';
+import 'package:responsive_dash_board/widgets/custom_dot_indicator.dart';
 import 'package:responsive_dash_board/widgets/custom_drawer.dart';
 import 'package:responsive_dash_board/widgets/my_cards_page_view.dart';
 
@@ -20,7 +21,8 @@ class DashboardDesktopLayout extends StatelessWidget {
           ),
         ),
         SizedBox(width: 24),
-        Expanded(child: MyCardsPageView()),
+        // Expanded(child: MyCardsPageView()),
+        CustomDotIndicator(isActive: true),
       ],
     );
   }
