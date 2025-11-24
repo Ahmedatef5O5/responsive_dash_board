10d310532bdbf6a83e1554a5c95025f92279e36e
Ahmedatef5O5 <ahmedateif11@gmail.com>
2025-11-24 20:56:43 +0200

    mobile layout part two

--- lib/views/dash_board_view.dart
+++ lib/views/dash_board_view.dart
@@ -3,6 +3,7 @@ import 'package:responsive_dash_board/layouts/dashboard_desktop_layout.dart';
 import 'package:responsive_dash_board/layouts/dashboard_mobile_layout.dart';
 import 'package:responsive_dash_board/layouts/dashboard_tablet_layout.dart';
 import 'package:responsive_dash_board/widgets/adaptive_layout.dart';
+import 'package:responsive_dash_board/widgets/custom_drawer.dart';
 
 class DashBoardView extends StatelessWidget {
   const DashBoardView({super.key});
@@ -10,12 +11,27 @@ class DashBoardView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: MediaQuery.sizeOf(context).width < 800
+          ? SizedBox(
+              width: MediaQuery.sizeOf(context).width * 0.7,
+              child: const CustomDrawer(),
+            )
+          : null,
       appBar: MediaQuery.of(context).size.width < 800
           ? AppBar(
               elevation: 0,
               surfaceTintColor: Colors.transparent,
               backgroundColor: Color(0xfffafafa),
-              leading: Icon(Icons.menu),
+              leading: Builder(
+                builder: (BuildContext context) {
+                  return GestureDetector(
+                    onTap: () {
+                      Scaffold.of(context).openDrawer();
+                    },
+                    child: Icon(Icons.menu),
+                  );
+                },
+              ),
             )
           : null,
       backgroundColor: Color(0xfff7f9fa),
