0cf2677e147eddb6c0c43384e189cc0f70ce294d
Ahmedatef5O5 <ahmedateif11@gmail.com>
2025-11-24 16:42:52 +0200

    create income chart

--- lib/widgets/income_section.dart
+++ lib/widgets/income_section.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/custom_background_container.dart';
+import 'package:responsive_dash_board/widgets/income_chart.dart';
 import 'package:responsive_dash_board/widgets/income_details.dart';
 import 'package:responsive_dash_board/widgets/income_section_header.dart'
     show IncomeSectionHeader;
@@ -14,7 +15,12 @@ class IncomeSection extends StatelessWidget {
         children: [
           IncomeSectionHeader(),
           Expanded(
-            child: Row(children: [Expanded(child: IncomeDetails())]),
+            child: Row(
+              children: [
+                Expanded(child: IncomeChart()),
+                // Expanded(child: IncomeDetails()),
+              ],
+            ),
           ),
         ],
       ),
