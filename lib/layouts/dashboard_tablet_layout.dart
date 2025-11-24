5c4a4a5ae16c0a38136cef5e137f72c72542c063
Ahmedatef5O5 <ahmedateif11@gmail.com>
2025-11-24 20:17:25 +0200

    tablet layout part two

--- lib/layouts/dashboard_tablet_layout.dart
+++ lib/layouts/dashboard_tablet_layout.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/all_expenses_and_quick_invoice_section.dart';
 import 'package:responsive_dash_board/widgets/custom_drawer.dart';
+import 'package:responsive_dash_board/widgets/income_section.dart';
+import 'package:responsive_dash_board/widgets/my_cards_and_transaction_history.dart';
 
 class DashboardTabletLayout extends StatelessWidget {
   const DashboardTabletLayout({super.key});
@@ -11,7 +13,23 @@ class DashboardTabletLayout extends StatelessWidget {
       children: [
         Expanded(child: CustomDrawer()),
         SizedBox(width: 32),
-        Expanded(flex: 3, child: AllExpensesAndQuickInvoiceSection()),
+        Expanded(
+          flex: 3,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                AllExpensesAndQuickInvoiceSection(),
+                SizedBox(height: 24),
+                MyCardsAndTransactionHistory(),
+
+                SizedBox(height: 24),
+                IncomeSection(),
+
+                SizedBox(height: 24),
+              ],
+            ),
+          ),
+        ),
         SizedBox(width: 32),
       ],
     );
