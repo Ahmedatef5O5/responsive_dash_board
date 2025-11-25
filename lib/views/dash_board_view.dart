9617941fffd1115e701a47a2abca92604581c0a9
Ahmedatef5O5 <ahmedateif11@gmail.com>
2025-11-25 13:07:28 +0200

    size config class

--- lib/views/dash_board_view.dart
+++ lib/views/dash_board_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/layouts/dashboard_desktop_layout.dart';
 import 'package:responsive_dash_board/layouts/dashboard_mobile_layout.dart';
 import 'package:responsive_dash_board/layouts/dashboard_tablet_layout.dart';
+import 'package:responsive_dash_board/utils/size_config.dart';
 import 'package:responsive_dash_board/widgets/adaptive_layout.dart';
 import 'package:responsive_dash_board/widgets/custom_drawer.dart';
 
@@ -10,14 +11,15 @@ class DashBoardView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SizeConfig.init(context);
     return Scaffold(
-      drawer: MediaQuery.sizeOf(context).width < 800
+      drawer: MediaQuery.sizeOf(context).width < SizeConfig.tablet
           ? SizedBox(
               width: MediaQuery.sizeOf(context).width * 0.7,
               child: const CustomDrawer(),
             )
           : null,
-      appBar: MediaQuery.of(context).size.width < 800
+      appBar: MediaQuery.of(context).size.width < SizeConfig.tablet
           ? AppBar(
               elevation: 0,
               surfaceTintColor: Colors.transparent,
--- lib/widgets/income_section.dart
+++ lib/widgets/income_section.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/custom_background_container.dart';
-import 'package:responsive_dash_board/widgets/income_chart.dart';
-import 'package:responsive_dash_board/widgets/income_details.dart';
+import 'package:responsive_dash_board/widgets/income_section_body.dart';
 import 'package:responsive_dash_board/widgets/income_section_header.dart'
     show IncomeSectionHeader;
 
@@ -13,22 +12,5 @@ class IncomeSection extends StatelessWidget {
     return CustomBackgroundContainer(
       child: Column(children: [IncomeSectionHeader(), IncomeSectionBody()]),
     );
-  }
-}
-
-class IncomeSectionBody extends StatelessWidget {
-  const IncomeSectionBody({super.key});
-  @override
-  Widget build(BuildContext context) {
-    double width = MediaQuery.sizeOf(context).width;
-    return width >= 1300 && width < 1750
-        ? SizedBox()
-        : Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Expanded(child: IncomeChart()),
-              Expanded(flex: 2, child: IncomeDetails()),
-            ],
-          );
   }
 }
