f9d50e0f7d376f78e8369318ddfaf69170e9ac8f
Ahmedatef5O5 <ahmedateif11@gmail.com>
2025-11-25 18:25:52 +0200

    responsive moblie layout part one

--- lib/widgets/all_expenses_item_header.dart
+++ lib/widgets/all_expenses_item_header.dart
@@ -15,22 +15,28 @@ class AllExpensesItemHeader extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Container(
-          width: 60,
-          height: 60,
-          padding: const EdgeInsets.all(
-            14,
-          ), // or put SvgPicture.asset(image) in Center Widget...
+        Flexible(
+          child: AspectRatio(
+            aspectRatio: 1,
+            child: Container(
+              width: 60,
+              height: 60,
+              // padding: const EdgeInsets.all(14), this make SVG icons disappear..
 
-          decoration: ShapeDecoration(
-            color: imageBackgroundC ?? Color(0xFFFAFAFA),
-            shape: OvalBorder(),
-          ),
-          child: SvgPicture.asset(
-            image,
-            colorFilter: ColorFilter.mode(
-              imageColor ?? const Color(0xff4eb7f2),
-              BlendMode.srcIn,
+              //  instead of padding  put SvgPicture.asset(image) in Center Widget...
+              decoration: ShapeDecoration(
+                color: imageBackgroundC ?? Color(0xFFFAFAFA),
+                shape: OvalBorder(),
+              ),
+              child: Center(
+                child: SvgPicture.asset(
+                  image,
+                  colorFilter: ColorFilter.mode(
+                    imageColor ?? const Color(0xff4eb7f2),
+                    BlendMode.srcIn,
+                  ),
+                ),
+              ),
             ),
           ),
         ),
